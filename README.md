908bb7151ec28c90fbce20accd8eba1bfa97a2b7
michal.penczek <michal.penczek@gmail.com>
2023-02-15 18:09:41 +0100

    test git ignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# ScorersReporter
+Web application for uploading .csv file with FIFA World Cup scorers saving it to SQL server.
---
+++ Services/IScorersReporterService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScorersReporter.Models;
+
+namespace ScorersReporter.Services
+{
+    public interface IScorersReporterService
+    {
+        public IEnumerable<T> SaveToDatabase<T>(Stream file);
+        public Task<List<DbScorer>> DatabaseReport();
+        public List<LeagueScorer> LeagueReport(string league);
+        public List<TopScorer> TopScorerReport();
+        public List<CCScorer> Top5CCS();
+        public void DownloadCsvFile();
+    }
+}
